aea48c903ba1e90339c2b1760a1b33cf6020663f
javedkhan8833 <55571765+javedkhan8833@users.noreply.github.com>
2021-04-29 15:20:06 +0500

    customize the product form

--- backend/views/product/_form.php
+++ backend/views/product/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
+use dosamigos\ckeditor\CKEditor;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Products */
@@ -14,21 +15,36 @@ use yii\bootstrap4\ActiveForm;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'description')->widget(CKEditor::className(), [
+        'options' => ['rows' => 6],
+        'preset' => 'basic'
+    ]) ?>
 
-    <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'image',[
 
-    <?= $form->field($model, 'price')->textInput(['maxlength' => true]) ?>
+        'template'=>'
+        <div class="custom-file">
+                {input}
+                {label}
+                {error}
+        </div>
+         ',
+         'labelOptions'=>['class'=>'custom-file-label'],
+         'inputOptions'=>['class'=>'custom-file-input']
 
-    <?= $form->field($model, 'status')->textInput() ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
+    ])->fileInput() ?>
 
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+    
 
-    <?= $form->field($model, 'created_by')->textInput() ?>
+    <?= $form->field($model, 'price')->textInput([
+        'maxlength' => true,
+        'type'=>'number'
+]) ?>
 
-    <?= $form->field($model, 'updated_by')->textInput() ?>
+    <?= $form->field($model, 'status')->checkBox() ?>
+
+    
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
